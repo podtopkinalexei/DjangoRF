ef487356259c285ac34eb72b949a79977308fc1c
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-09-27 18:13:56 +0300

    Добавлены пути materials

---
+++ materials/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CourseViewSet, LessonListCreateAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, LessonDestroyAPIView
+
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('lessons/', LessonListCreateAPIView.as_view(), name='lesson-list-create'),
+    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-detail'),
+    path('lessons/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lessons/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+]
