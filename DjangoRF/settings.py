8ddb29b3ae78794df876993ab357e7e2e1ff693b
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-09-27 18:41:20 +0300

    Обновлены настройки REST_FRAMEWORK

--- DjangoRF/settings.py
+++ DjangoRF/settings.py
@@ -91,5 +91,9 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
