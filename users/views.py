75f96260c97cbad2b04fb8a8ac2ad8a8b8647fd5
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-09-27 18:39:54 +0300

    Добавлен контроллер для Платежей

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from .models import Payment
+from .serializers import PaymentSerializer
 
-# Create your views here.
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = [
+        'paid_course',
+        'paid_lesson',
+        'payment_method'
+    ]
+    ordering_fields = ['payment_date']
