bfd073575209a6f2e708d577f0240abfd61119e1
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-10-05 18:11:50 +0300

    Обновлены права доступа для моделей Materials

--- materials/models.py
+++ materials/models.py
@@ -1,14 +1,19 @@
 from django.db import models
-
+from users.models import User
 
 class Course(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
     preview = models.ImageField(upload_to='courses/previews/', blank=True, null=True, verbose_name='Превью')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True,
+                             verbose_name='Владелец', related_name='courses')
 
     class Meta:
         verbose_name = 'Курс'
         verbose_name_plural = 'Курсы'
+        permissions = [
+            ("can_edit_course", "Can edit course"),
+        ]
 
     def __str__(self):
         return self.title
@@ -20,11 +25,15 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to='lessons/previews/', blank=True, null=True, verbose_name='Превью')
     video_link = models.URLField(blank=True, null=True, verbose_name='Ссылка на видео')
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name='Курс')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True,
+                             verbose_name='Владелец', related_name='lessons')
 
     class Meta:
         verbose_name = 'Урок'
         verbose_name_plural = 'Уроки'
+        permissions = [
+            ("can_edit_lesson", "Can edit lesson"),
+        ]
 
     def __str__(self):
         return self.title
-    
