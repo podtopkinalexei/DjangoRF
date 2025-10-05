62aa20c08bdd37d23975560fa1c52032337f43ea
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-10-05 18:12:49 +0300

    Обновлены сериализаторы Materials

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,18 +3,24 @@ from .models import Course, Lesson
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    owner_email = serializers.EmailField(source='owner.email', read_only=True)
+
     class Meta:
         model = Lesson
         fields = '__all__'
+        read_only_fields = ['owner']
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
+    owner_email = serializers.EmailField(source='owner.email', read_only=True)
 
     class Meta:
         model = Course
         fields = '__all__'
+        read_only_fields = ['owner']
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
+
