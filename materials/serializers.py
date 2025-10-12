9add181f88308fe526d8c38bf918fad4b4e16a5b
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-10-12 12:01:43 +0300

    Обновлены сериализаторы materials

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Course, Lesson
+from .models import Course, Lesson, Subscription
+from .validators import YouTubeLinkValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -10,11 +11,23 @@ class LessonSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ['owner']
 
+        validators = [
+            YouTubeLinkValidator(field='video_link')
+        ]
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
+        read_only_fields = ['user', 'subscribed_at']
+
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
     owner_email = serializers.EmailField(source='owner.email', read_only=True)
+    is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
@@ -23,4 +36,16 @@ class CourseSerializer(serializers.ModelSerializer):
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
+
+    def get_is_subscribed(self, obj):
+        """
+        Проверяет, подписан ли текущий пользователь на курс
+        """
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return Subscription.objects.filter(
+                user=user,
+                course=obj
+            ).exists()
+        return False
 
