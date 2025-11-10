4b0e745a7e100fbd53a666b847d0e698a4d6a076
podtopkinalexei <podtopkinalexei@gmail.com>
2025-11-10 18:21:02 +0300

    Добавлена переменная разрешенных hosts

--- DjangoRF/settings.py
+++ DjangoRF/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web', '0.0.0.0']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
